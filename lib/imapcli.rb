12d4392460125f8bfb2bb4ada42ac10352f311e7
Nicolas Rodriguez <nico@nicoladmin.fr>
2024-10-24 16:08:57 +0200

    Extract Imapcli::CLI class

--- lib/imapcli.rb
+++ lib/imapcli.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
+# require ruby dependencies
+require 'csv'
+# require 'pp'
+
 # require external dependencies
+# require 'pry'
+require 'gli'
+require 'dotenv'
+require 'tty-prompt'
+require 'tty-table'
+require 'tty-progressbar'
 require 'zeitwerk'
 
 # load zeitwerk
 Zeitwerk::Loader.for_gem.tap do |loader|
   loader.setup
+  loader.inflector.inflect 'cli' => 'CLI'
 end
 
 module Imapcli
